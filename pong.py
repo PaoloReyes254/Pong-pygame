feb04d2036b1f6e3a71dcf91fa348ad160b4aead
PaoloReyes254 <ceo@paoloinspires.com>
2020-12-27 22:37:45 -0600

    Random ball reset direction

--- pong.py
+++ pong.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 #Classes
 class PLAYER(object):
@@ -16,7 +16,21 @@ class OPPONENT(PLAYER):
 class BALL(object):
     def __init__(self):
         self.radius = 8
-        self.ball_pos = (800/2, 600/2)
+        self.ball_x = 800/2
+        self.ball_y = 600/2
+        self.ball_pos = (self.ball_x, self.ball_y)
+        self.speed_x = 2
+        self.speed_y = 2
+        self.reset()
+
+    def move_ball(self):
+        self.ball_y += self.random_y_speed
+        self.ball_x += self.random_x_speed
+        self.ball_pos = (self.ball_x, self.ball_y)
+    
+    def reset(self):
+        self.random_x_speed = random.choice([-self.speed_x, self.speed_x])
+        self.random_y_speed = random.choice([-self.speed_y, self.speed_y, 0])
 
 class MAIN(object):
     def __init__(self):
@@ -25,6 +39,7 @@ class MAIN(object):
         self.ball = BALL()
 
     def draw_objects(self):
+        screen.fill((0, 0, 0))
         pygame.draw.rect(screen, (254, 254, 254), self.player.rect)
         pygame.draw.rect(screen, (254, 254, 254), self.opponent.rect)
         pygame.draw.circle(screen, (254, 254, 254), self.ball.ball_pos, self.ball.radius, 0)
@@ -40,7 +55,7 @@ width = 800
 height = 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pong")
-logo_surface = pygame.image.load("assets/ponglogo.jpg").convert_alpha()
+logo_surface = pygame.image.load("assets/ponglogo.jpg").convert()
 pygame.display.set_icon(logo_surface)
 clock = pygame.time.Clock()
 
@@ -51,6 +66,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    game_main.ball.move_ball()
     game_main.draw_objects()
 
     pygame.display.update()
